3ca29546538ea4fcbcb18f27343772ace6c782e8
mochung <mo611003@hotmail.com.tw>
2014-11-02 04:18:06 -0800

    3 unfinish, but solve string format

--- 3/commission.h
+++ 3/commission.h
@@ -14,29 +14,27 @@ char* commission(int locks, int stocks, int barrels)
 	stockSales = stocks * stockPrice;
 	barrelSales = barrels * barrelPrice;
 	totalSales = lockSales + stockSales + barrelSales;
-	
+/*
 	if (locks > 70 || locks < 1) {
 		sprintf(result, "locks not in 1...70");
 	}
 	else if (stocks > 80 || stocks < 1) {
-
 		sprintf(result, "stocks not in 1...80");
 	}
 	else if (barrels > 90 || barrels < 1) {
-
 		sprintf(result, "barrels not in 1...90");
 	}
-	else if (totalSales > 1800.0) {
+	else*/ if (totalSales > 1800.0) {
 		commission = 1000.0 * 0.1 + 800.0 * 0.15 + (totalSales - 1800.0) * 0.2;
-		sprintf(result, "$%lf", commission);
+		sprintf(result, "$%g", commission);
 	}
 	else if (totalSales > 1000.0) {
 		commission = 1000.0 * 0.1 + (totalSales - 1000.0) * 0.15;
-		sprintf(result, "$%lf", commission);
+		sprintf(result, "$%g", commission);
 	}
 	else {
 		commission = totalSales * 0.1;
-		sprintf(result, "$%lf", commission);
+		sprintf(result, "$%g", commission);
 	}
 
 	return result;
